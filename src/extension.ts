f3ca4518270370eea954418728b5328f2cd2ab21
ustc-q <55303382+ustc-q@users.noreply.github.com>
2023-07-17 21:41:11 +0800

    prettier; husky; lint-stage

--- src/extension.ts
+++ src/extension.ts
@@ -13,15 +13,21 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('jim-vsc-plugin-demo.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('jim-vsc-plugin-demo.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from jim-vsc-plugin-demo!');
-    console.log('command jim-vsc-plugin-demo.helloWorld triggered!');
+		console.log('command jim-vsc-plugin-demo.helloWorld triggered!');
 	});
+	const obj = {
+		name: 'jack',
+	};
+	console.log('obj', obj);
 
 	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	console.log('deactivate');
+}
