c902271b7579e6b0deb26f0122645278a1b19416
jim <zoujinqiang@ultrarisc.com>
2023-08-01 18:38:08 +0800

    feat: browse lite

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,14 @@
 import * as vscode from 'vscode';
 import Movies from "./webviews/WebviewMovieList";
 import News from './webviews/WebviewNewsList';
+import UseBrowserLiteCmd from './commands/UseBrowserLiteCmd';
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('jim-vsc-plugin-demo.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World from jim-vsc-plugin-demo!');
 		console.log('command jim-vsc-plugin-demo.helloWorld triggered!');
 	}));
+	new UseBrowserLiteCmd(context);
 	new Movies(context);
 	new News(context);
 }
